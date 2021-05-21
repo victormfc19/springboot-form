8f49832c8358ec3fe19da4c9981c41cea49c12a7
Victor <victor_florez82171@elpoli.edu.co>
2021-05-20 23:13:10 -0500

    se agrega clase model, pagina de respuesta y validaciones

--- src/main/java/com/example/demo/controllers/FormController.java
+++ src/main/java/com/example/demo/controllers/FormController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.domain.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 public class FormController {
@@ -16,15 +19,9 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String processForm(Model model,
-                @RequestParam String username,
-                @RequestParam String password,
-                @RequestParam String email
-                              ){
+    public String processForm(@Valid Usuario usuario, Model model){
         model.addAttribute("titulo", "Formulario usuarios");
-        model.addAttribute("username",username);
-        model.addAttribute("password",password);
-        model.addAttribute("email",email);
+        model.addAttribute("usuario",usuario);
         return "response";
     }
 }
